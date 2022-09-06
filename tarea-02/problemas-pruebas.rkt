85ee89b820a19ecfe4400fc7e17fbd7d8b74ed2e
Fermin A. Ahumada Garcia <a220207901@unison.mx>
2022-09-05 21:38:47 -0700

    Aqui andamos,por que aqui nos puso la vida

---
+++ tarea-02/problemas-pruebas.rkt
@@ -0,0 +1,67 @@
+#lang racket
+(require rackunit
+         rackunit/text-ui
+         "problemas.rkt")
+
+(define-test-suite pruebas
+ (test-case "bundle"
+             (check-equal? (bundle (explode "abcdefg") 3)
+                           (list "abc" "def" "g"))
+             (check-equal? (bundle (explode "abcdefgh") 2)
+                           (list "ab" "cd" "ef" "gh"))
+             (check-equal? (bundle (explode "abcdefgh") 1)
+                           (list "a" "b" "c" "d" "e" "f" "g" "h"))
+             (check-equal? (bundle '() 2)
+                           '())
+             (check-equal? (bundle '("a" "b") 3)
+                           (list "ab"))
+             (check-exn exn:fail? (thunk (bundle '("") 3)))
+             (check-exn exn:fail? (thunk (bundle (explode "abcdefgh") 0))))
+
+ 
+  (test-case "take"
+             (check-equal? (take '(1 2 3 4 5 6) 2)
+                           '(1 2))
+             (check-equal? (take '("a" "b" "c" "d" "e") 3)
+                           '("a" "b" "c"))
+             (check-equal? (take '() 3)
+                           '())
+             (check-equal? (take '(1 2) 7)
+                           '(1 2)))
+
+(test-case "drop"
+             (check-equal? (drop '(1 2 3 4 5 6) 3)
+                           '(4 5 6))
+             (check-equal? (drop '("a" "b" "c" "d" "e") 3)
+                           '("d" "e"))
+             (check-equal? (drop '() 3)
+                           '())
+             (check-equal? (drop '(1 2) 3)
+                           '()))
+ (test-case "list->chunks"
+             (check-equal? (list->chunks '() 0) '())
+             (check-equal? (list->chunks '(2 4 6 8 10 12) 2) '((2 4) (6 8) (10 12)))
+             (check-equal? (list->chunks '(2 8 76 89 23 12) 0) '())
+             (check-equal? (list->chunks '(2 4 6 8 10 12 14) 3) '((2 4 6) (8 10 12) (14)))
+             )
+
+   (test-case "bundle-chunk"
+             (check-equal? (bundle-chunk (explode "abcdefg") 3)
+                           (list "abc" "def" "g"))
+             (check-equal? (bundle-chunk (explode "abcdefgh") 2)
+                           (list "ab" "cd" "ef" "gh"))
+             (check-equal? (bundle-chunk (explode "abcdefgh") 1)
+                           (list "a" "b" "c" "d" "e" "f" "g" "h"))
+             (check-equal? (bundle-chunk '() 2)
+                           '())
+             (check-equal? (bundle-chunk '("a" "b") 3)
+                           (list "ab")))
+
+  (test-case "partition"
+             (check-equal? (partition "" 3) '())
+             (check-equal? (partition "Benito Perez" 0) '())
+             (check-equal? (partition "Veneco" 2) '("Ve" "ne" "co"))
+             (check-equal? (partition ":)" 3) '(":)")))
+             (check-equal? (partition "Venecos" 3) '("Ven" "eco" "s"))
+  )
+(run-tests pruebas 'verbose)
--- tarea-02/problemas.rkt
+++ tarea-02/problemas.rkt
@@ -1,51 +1,105 @@
-#lang plait
+#lang racket
 
-(define-type ArithC
-  [numC (num : Number)]
-  [plusC (l : ArithC) (r : ArithC)]
-  [multC (l : ArithC) (r : ArithC)])
+(define (unit-string? x)
+  (and (string? x)
+       (= (string-length x) 1)))
 
-(define-type ArithS
-  [numS (num : Number)]
-  [plusS (l : ArithS) (r : ArithS)]
-  [multS (l : ArithS) (r : ArithS)]
-  [negS (e : ArithS)]
-  [minuS (l : ArithS) (r : ArithS)])
+(define (unit-string-list? x)
+  (or (null? x)
+      (and (pair? x)
+           (string? (first x))
+           (= (string-length (first x)) 1)
+           (unit-string-list? (rest x)))))
 
+(define (explode s)
+  (unless (string? s)
+    (error 'explode "esperaba una cadena, pero recibí: ~e" s))
+  (map string (string->list s)))
 
-(define (interp [a : ArithC]) : Number
-  (type-case ArithC a
-    [(numC n) (n)]
-    [(plusC l r) (+ (interp (plusC-l a)) (interp (plusC-r a)))]
-    [(multC l r) (* (interp (multC-l a)) (interp (multC-r a)))]))
+(define (implode ls)
+  (unless (unit-string-list? ls)
+    (error 'implode "esperaba una lista de cadenas unitarias, pero recibí: ~e" ls))
+  (apply string-append ls))
 
-(define (desugar [s : ArithS]) : ArithC
-  (type-case ArithS s
-    [(numS n) (numC n)]
-    [(plusS l r) (plusC (desugar l) (desugar r))]
-    [(multS l r) (multC (desugar l) (desugar r))]
-    [(minuS l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
-    [(negS x) (multC (numC -1) (desugar x))]))
-
-(define (eval [input : S-Exp]) : Number
-  (interp (desugar (parse input))))
-
-
-(define (parse [s : S-Exp]) : ArithS
+(define (bundle s n)
   (cond
-    [(s-exp-number? s) (numS (s-exp->number s))]
-    [(and (s-exp-list? s) (not (equal? s `{})))
-     (let ([lst (s-exp->list s)])
-       (cond
-         [(not (s-exp-symbol? (first lst))) (error 'parse "operacion aritmetica no es prefix.")]
-         [(= (length lst) 2)
-          (case (s-exp->symbol (first lst))
-            ['- (minuS (parse (second lst)))])]
-         [(= (length lst) 3)
-          (case (s-exp->symbol (first lst))
-            ['+ (plusS (parse (second lst)) (parse (third lst)))]
-            ['* (multS (parse (second lst)) (parse (third lst)))]
-            ['- (negS (parse (second lst)) (parse (third lst)))]
-            [else (error 'parse "no se reconoce el simbolo en la operacion aritmetica.")])]
-         [else (error 'parse "operacion aritmetica malformada.")]))]
-     [else (error 'parse "expresion aritmetica malformada.")]))
+    [(and (> n 0) (list? s))
+    (cond
+      [(or (null? s) (or (zero? n) (< n 0))) s]
+      [else
+       (cons (implode (take s n))
+             (bundle (drop s n) n))])]
+     [else (error 'bundle "n es menor que cero o no pasaste una lista")]))
+
+#|PROBLEMA 3
+1. Implementa el procedimiento (take l n) la cuál regresa una lista de los primeros n elementos
+de l, o cuantos elementos haya si l tiene menos de n elementos.
+2. Implementa el procedimiento (drop l n) la cuál regresa una lista con los elementos de l
+excepto los primeros n, o la cadena vacía si l tiene menos de n elementos.|#
+(define (take l n)
+(cond
+[(>= n (length l)) l]
+[(= n 0) '()]
+[else  (cons (first l) (take (rest l) (- n 1)))]))
+
+(define (drop l n)
+(cond
+[(>= n (length l)) '()]
+[(= n 0) (cons (first l) (drop (rest l) n))]
+[else  (drop (rest l) (- n 1))]))
+#|PROBLEMA 6
+Define el procedimiento list->chunks. Consume una lista l de valores arbitrarios y un
+natural n. El resultado es una lista de trozos de tamaño n. Cada trozo representa una sub-secuencia
+de elementos en l. Implementa bundle usando list->chunks.
+|#
+(define (list->chunks ls n)
+  (cond
+    [(or(empty? ls) (zero? n)) null]
+    [else (cons (take ls n) (list->chunks (drop ls n) n))]))
+
+(define (bundle-chunk s n)
+ (cond
+   [(or (null? s) (or (zero? n) (< n 0))) s]
+   [else (let add ([ls (list->chunks s n)])
+           (cond
+             [(empty? ls) null]
+             [else (cons (implode (first ls)) (add (rest ls)))]))]))
+#|PROBLEMA 7
+Define partition. Toma una cadena s y un natural n. Produce una lista de trozos de
+cadenas de tamaño n. Para cadenas no vacías s y enteros positivos n se tiene que
+|#
+
+(define (partition s n)
+  (cond
+    [(<= n 0) null]
+    [(> n 0) (if(> n (string-length s))
+          (if (eq? (string-length s) 0)
+              null
+              (list s))
+          (cons (substring s 0 n) (partition (substring s n) n)))]
+    ))
+#|2.2 Recursión que ignora estructura
+PROBLEMA 8
+Modifica esos procedimientos para poder ordenar en orden ascendente cualquier tipo
+de valor a partir de un argumento adicional.
+|#
+(define (isort ls)
+  (if(empty? ls)
+     null
+     (insert (first ls)(isort (rest ls)))))
+
+(define (insert n ls)
+  (cond
+    [(empty? ls) (list n)]
+    [(<= n (first ls)) (cons n ls)]
+    [else (cons (first ls) (insert n (rest ls)))]))
+
+#|PROBLEMA 10
+Implementa los procedimientos smallers y largers, son recursivos estructurales.
+Uno debe regresar los elementos que son estríctamente menores al pivote y otro debe regresar los
+elementos que son estríctamente mayores al pivote.
+|#
+
+
+
+(provide (all-defined-out))
